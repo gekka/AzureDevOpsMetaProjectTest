eb4e894d55efae029bea553f173906ed2cb4e452
gekka <10055381+gekka@users.noreply.github.com>
2026-02-16 15:42:27 +0900

    Init

---
+++ AzureDevOpsMetaProjectTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AzureDevOpsMetaProjectTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
